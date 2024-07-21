a385ed31e84fdddf1fcbc02574434d406377c6ec
leoferson07 <leofersontp0912@gmail.com>
2024-07-21 19:05:19 -0400

    configuraciones

--- routes/auth.js
+++ routes/auth.js
@@ -2,7 +2,7 @@ const express = require('express');
 const router = express.Router();
 const loginController = require("../controladores/auth");
 
-
+//vista
 router.get('/', (req, res) => {
     res.render('login');
   });
